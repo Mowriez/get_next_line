e5cea317dc7ee7933122ded488fa622ca00c9a89
Maurice <mauricetrautner@gmail.com>
2022-10-27 23:51:26 +0200

    final version w/o bonus

---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtrautne <mtrautne@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/27 13:43:59 by mtrautne          #+#    #+#             */
+/*   Updated: 2022/10/27 23:45:18 by mtrautne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 50
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_read(int fd, char *static_str);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_next_line(char *static_str);
+char	*ft_resize_static_str(char *static_str);
+
+#endif
