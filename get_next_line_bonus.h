d950ce17f77ba0e76448596711fa012f1c76a56e
mtrautne <mtrautne@student.42wolfsburg.de>
2022-10-28 10:46:49 +0200

    Bonus added.

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtrautne <mtrautne@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/27 13:43:59 by mtrautne          #+#    #+#             */
+/*   Updated: 2022/10/28 10:35:02 by mtrautne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 50
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_read(int fd, char *static_str);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_next_line(char *static_str);
+char	*ft_resize_static_str(char *static_str);
+
+#endif
